814f999d83bf12540fb563f5ff4d17d8ec02f393
estymator <pawel.chelmecki55@gmail.com>
2019-11-25 02:27:47 +0100

    Post personel

--- src/main/java/hotels/Hotel.java
+++ src/main/java/hotels/Hotel.java
@@ -1,22 +1,12 @@
 package hotels;
 
+
+
 public class Hotel {
-    private final long id;
-    private final String nazwa;
+    private long id;
+    private String nazwa;
 
 
 
-    public Hotel(long id, String n)
-    {
-        this.id=id;
-        this. nazwa=n;
-    }
 
-    public long getId() {
-        return id;
-    }
-
-    public String getNazwa() {
-        return nazwa;
-    }
 }
---
+++ src/main/java/hotels/PersonelRepository.java
@@ -0,0 +1,8 @@
+package hotels;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface PersonelRepository extends CrudRepository<Personel, Integer> {
+}
