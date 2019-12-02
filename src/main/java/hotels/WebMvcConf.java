24281e95b56e4e5c4c93aa57a166a4b6351e1d2a
estymator <pawel.chelmecki55@gmail.com>
2019-12-02 01:06:42 +0100

    add logs

--- src/main/java/hotels/WebMvcConf.java
+++ src/main/java/hotels/WebMvcConf.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConf implements  WebMvcConfigurer{
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
